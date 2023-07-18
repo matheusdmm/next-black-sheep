31704bead4e61f2f9a1f66438930a8e4156e256c
Matheus 🧑‍🚀 <matheusdmm@users.noreply.github.com>
2023-07-18 16:23:48 -0300

    404 page

---
+++ app/404.tsx
@@ -0,0 +1,13 @@
+export default function NotFound() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="space-y-4 text-center">
+        <h1 className="text-6xl font-bold tracking-tight text-white sm:text-[5rem]">
+          <span className="block">Go back fam</span>
+          <span className="block">Nothing here for ya!</span>
+          <span className="block">404 not found</span>
+        </h1>
+      </div>
+    </main>
+  );
+}
